da4fd19266dc941b04728204e12ffc0da65130cf
Kimia <kimia.ehsani.dolati.al@iespoligonosur.org>
2021-03-20 20:27:32 +0100

    Completada2

--- src/main/java/clases/Controller.java
+++ src/main/java/clases/Controller.java
@@ -5,7 +5,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
 public class Controller {
+	private double num1D = 0, num2D = 0;
 
 	@FXML
 	private TextField resulOp;
@@ -22,8 +24,63 @@ public class Controller {
 
 	@FXML
 	private void plus(ActionEvent event) throws IOException {
+
+		// Si los números introducido son validos calculamos la operacion con la ayuda
+		// de la clase Calculadora
+		if (validacionConversionNum()) {
+			labelError.setText("");
+			Calculadora c = new Calculadora();
+			double resul = c.sumar(num1D, num2D);
+			resulOp.setText(resul + "");
+		}
+
+	}
+
+	@FXML
+	private void restar(ActionEvent event) throws IOException {
+
+		if (validacionConversionNum()) {
+			labelError.setText("");
+			Calculadora c = new Calculadora();
+			double resul = c.restar(num1D, num2D);
+			resulOp.setText(resul + "");
+		}
+
+	}
+
+	@FXML
+	private void mul(ActionEvent event) throws IOException {
+
+		if (validacionConversionNum()) {
+			labelError.setText("");
+			Calculadora c = new Calculadora();
+			double resul = c.multiplicar(num1D, num2D);
+			resulOp.setText(resul + "");
+		}
+
+	}
+
+	@FXML
+	private void div(ActionEvent event) throws IOException {
+
+		if (validacionConversionNum()) {
+			labelError.setText("");
+			Calculadora c = new Calculadora();
+			double resul = c.dividir(num1D, num2D);
+			resulOp.setText(resul + "");
+		}
+
+	}
+
+	/**
+	 * Este método convierte los strings obtenidos de los textFiedls y los convierte
+	 * en double y controla los errores posibles a la hora de conversion.
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
+	private boolean validacionConversionNum() throws IOException {
 		boolean valido = true;
-		double num1D = 0, num2D = 0;
 		// Asegurandonos que se introduce algo en el textField
 		if (num1.getText() != null && num2.getText() != null) {
 			// Asegurandonos que se introduce un numero en el textField
@@ -42,116 +99,13 @@ public class Controller {
 					labelError.setText("Error: Debes introducir dos números!!");
 					valido = false;
 				}
-
-				// Calculando la operacion con la ayuda de la clase Calculadora
-				if (valido) {
-					labelError.setText("");
-					Calculadora c = new Calculadora();
-					double resul = c.sumar(num1D, num2D);
-					resulOp.setText(resul + "");
-				}
 			}
 		} else {
 			labelError.setText("Error: Debes introducir dos números!!");
 		}
+
+		return valido;
+
 	}
 
-	@FXML
-	private void restar(ActionEvent event) throws IOException {
-		boolean valido = true;
-		double num1D = 0, num2D = 0;
-		if (num1.getText() != null && num2.getText() != null) {
-			try {
-				num1D = Double.parseDouble(num1.getText());
-				valido = true;
-			} catch (NumberFormatException e) {
-				labelError.setText("Error: Debes introducir dos números!!");
-				valido = false;
-			}
-			if (valido) {
-				try {
-					num2D = Double.parseDouble(num2.getText());
-					valido = true;
-				} catch (NumberFormatException e) {
-					labelError.setText("Error: Debes introducir dos números!!");
-					valido = false;
-				}
-
-				if (valido) {
-					labelError.setText("");
-					Calculadora c = new Calculadora();
-					double resul = c.restar(num1D, num2D);
-					resulOp.setText(resul + "");
-				}
-			}
-		} else {
-			labelError.setText("Error: Debes introducir dos números!!");
-		}
-	}
-
-	@FXML
-	private void mul(ActionEvent event) throws IOException {
-		boolean valido = true;
-		double num1D = 0, num2D = 0;
-		if (num1.getText() != null && num2.getText() != null) {
-			try {
-				num1D = Double.parseDouble(num1.getText());
-				valido = true;
-			} catch (NumberFormatException e) {
-				labelError.setText("Error: Debes introducir dos números!!");
-				valido = false;
-			}
-			if (valido) {
-				try {
-					num2D = Double.parseDouble(num2.getText());
-					valido = true;
-				} catch (NumberFormatException e) {
-					labelError.setText("Error: Debes introducir dos números!!");
-					valido = false;
-				}
-
-				if (valido) {
-					labelError.setText("");
-					Calculadora c = new Calculadora();
-					double resul = c.multiplicar(num1D, num2D);
-					resulOp.setText(resul + "");
-				}
-			}
-		} else {
-			labelError.setText("Error: Debes introducir dos números!!");
-		}
-	}
-
-	@FXML
-	private void div(ActionEvent event) throws IOException {
-		boolean valido = true;
-		double num1D = 0, num2D = 0;
-		if (num1.getText() != null && num2.getText() != null) {
-			try {
-				num1D = Double.parseDouble(num1.getText());
-				valido = true;
-			} catch (NumberFormatException e) {
-				labelError.setText("Error: Debes introducir dos números!!");
-				valido = false;
-			}
-			if (valido) {
-				try {
-					num2D = Double.parseDouble(num2.getText());
-					valido = true;
-				} catch (NumberFormatException e) {
-					labelError.setText("Error: Debes introducir dos números!!");
-					valido = false;
-				}
-
-				if (valido) {
-					labelError.setText("");
-					Calculadora c = new Calculadora();
-					double resul = c.dividir(num1D, num2D);
-					resulOp.setText(resul + "");
-				}
-			}
-		} else {
-			labelError.setText("Error: Debes introducir dos números!!");
-		}
-	}
 }
